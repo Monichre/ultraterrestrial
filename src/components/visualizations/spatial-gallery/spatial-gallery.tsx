ee09f7848ead02119cc958f75dca7fa33db52f66
monichre <liamhellis@gmail.com>
2024-05-03 19:26:15 -0500

    wrote function for generating spatial positions when using full dataset

--- src/components/visualizations/spatial-gallery/spatial-gallery.tsx
+++ src/components/visualizations/spatial-gallery/spatial-gallery.tsx
@@ -14,21 +14,17 @@ import {
   MeshReflectorMaterial,
   Text,
   Environment,
-  View,
   Html,
   useTexture,
-  Image,
   Preload,
 } from '@react-three/drei'
-// import Image from 'next/image'
-import { useParams, usePathname, useRouter } from 'next/navigation'
-import { easing, geometry } from 'maath'
 
+import { useRouter, useSearchParams } from 'next/navigation'
+import { easing, geometry } from 'maath'
+import NextImage from 'next/image'
 extend(THREE)
 extend(geometry)
 extend({ RoundedPlaneGeometry: geometry.RoundedPlaneGeometry })
-
-// import { View } from '@/components/3d/canvas/View'
 
 extend({ RoundedPlaneGeometry: geometry.RoundedPlaneGeometry })
 
@@ -61,7 +57,6 @@ export const SpatialGallery = ({ items }: any) => {
           </mesh>
         </group>
         <Environment preset='city' />
-
         <Preload all />
       </Suspense>
     </Canvas>
@@ -76,12 +71,15 @@ function Frames({
   const ref: any = useRef()
   const clicked: any = useRef()
 
-  // const [, params] = useRoute('/item/:id')
   const router = useRouter()
-  const params = useParams()
+
+  const searchParams = useSearchParams()
+
+  const id = searchParams.get('id')
 
   useEffect(() => {
-    clicked.current = ref?.current?.getObjectByName(params?.id)
+    clicked.current = ref?.current?.getObjectByName(id)
+
     if (clicked.current) {
       clicked.current.parent.updateWorldMatrix(true, true)
       clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1.25))
@@ -100,7 +98,7 @@ function Frames({
   const handleClick = (e) => {
     e.stopPropagation()
     router.push(
-      clicked.current === e.object ? '/history' : '/item/' + e.object.id
+      clicked.current === e.object ? '/history' : '?id=' + e.object.name
     )
   }
   const handleHistory = () => {
@@ -108,7 +106,12 @@ function Frames({
   }
 
   return (
-    <group ref={ref} onClick={handleClick} onPointerMissed={handleHistory}>
+    <group
+      ref={ref}
+      onClick={handleClick}
+      onPointerMissed={handleHistory}
+      name={`parent-frames-group`}
+    >
       {items?.length
         ? items.map(
             (item: any) => <Frame key={item.id} item={item}  /> /* prettier-ignore */
@@ -118,80 +121,53 @@ function Frames({
   )
 }
 
-// const MyImage = ({ url, ...props }: any) => {
-//   const texture: any = useTexture(url)
-//   console.log('texture: ', texture)
-
-//   // useFrame((state, delta) => (scene.rotation.y += delta))
-//   return (
-//     <View>
-//       <primitive object={texture} position={[0, 0, 0.7]} {...props} />
-//       {/* @ts-ignore */}
-//       {/* <Image position={[0, 0, 0.7]} ref={ref} texture={texture} /> */}
-//     </View>
-//   )
-// }
-
-// extend({ MyImage })
-
 function Frame({ item, c = new THREE.Color(), ...props }) {
   const { position, rotation, ...rest } = item
-  const image = useRef()
+  const image: any = useRef()
   const frame: any = useRef()
-  const params = useParams()
-  // const { imageUrl } = useRemoteImage(item?.photo.url)
-  // console.log('imageUrl: ', imageUrl)
+
+  const searchParams = useSearchParams()
+
+  const id = searchParams.get('id')
 
   const [hovered, hover] = useState(false)
   const [rnd] = useState(() => Math.random())
+
   const name = item.name
-  const isActive = params === item.id
+  const isActive = id === item.id
   const proxyUrl = `/api/image?imageUrl=${item?.photo.url}`
   const texture: any = useTexture(proxyUrl)
 
+  useCursor(hovered)
   useFrame((state, dt) => {
-    // image.current.material.zoom = 2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
-    // easing.damp3(image.current.scale, [0.85 * (!isActive && hovered ? 0.85 : 1), 0.9 * (!isActive && hovered ? 0.905 : 1), 1], 0.1, dt)
+    if (image?.current?.material) {
+      image.current.material.zoom =
+        2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
+    }
+
+    easing.damp3(
+      image?.current?.scale,
+      [
+        0.85 * (!isActive && hovered ? 0.85 : 1),
+        0.9 * (!isActive && hovered ? 0.905 : 1),
+        1,
+      ],
+      0.1,
+      dt
+    )
+
     easing.dampC(
-      frame.current.material.color,
+      frame?.current?.material?.color,
       hovered ? 'orange' : 'white',
       0.1,
       dt
     )
-  })
-
-  // useCursor(hovered)
-  /* The `useFrame` hook in the provided code snippet is used to perform animations and updates on each
- frame rendered by the Three.js scene. Let's break down the code inside the `useFrame` hook: */
-  // useFrame((state, dt) => {
-  //   console.log('state: ', state)
-  //   console.log('image: ', image)
-  //   if (image?.current && frame?.current) {
-  //     image.current.material.zoom =
-  //       2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
-  //     easing.damp3(
-  //       image.current.scale,
-  //       [
-  //         0.85 * (!isActive && hovered ? 0.85 : 1),
-  //         0.9 * (!isActive && hovered ? 0.905 : 1),
-  //         1,
-  //       ],
-  //       0.1,
-  //       dt
-  //     )
-  //     easing.dampC(
-  //       frame.current.material.color,
-  //       hovered ? 'orange' : 'white',
-  //       0.1,
-  //       dt
-  //     )
-  //   }
-  // })
+  }, 1)
 
   return (
     <group rotation={rotation} position={position}>
       <mesh
-        name={item.id}
+        name={`${item.id}`}
         onPointerOver={(e) => (e.stopPropagation(), hover(true))}
         onPointerOut={() => hover(false)}
         scale={[1, GOLDENRATIO, 0.05]}
@@ -214,13 +190,23 @@ function Frame({ item, c = new THREE.Color(), ...props }) {
           <meshBasicMaterial toneMapped={false} fog={false} />
         </mesh>
         <Html
+          ref={image}
+          castShadow
+          receiveShadow
           transform
           position={[0, 0, 0.7]}
           raycast={() => null}
           scale={0.1}
           occlude='blending'
         >
-          <img src={proxyUrl} alt='test' height={400} width={400} />
+          <NextImage
+            src={proxyUrl}
+            alt='test'
+            height={300}
+            width={400}
+            style={{ objectFit: 'contain' }}
+          />
+          <p>{name.split('-').join(' ')}</p>
         </Html>
       </mesh>
       <Text
@@ -232,26 +218,15 @@ function Frame({ item, c = new THREE.Color(), ...props }) {
       >
         {name.split('-').join(' ')}
       </Text>
+      <Text
+        maxWidth={0.1}
+        anchorX='left'
+        anchorY='top'
+        position={[0.55, GOLDENRATIO, 0]}
+        fontSize={0.025}
+      >
+        {item.date}
+      </Text>
     </group>
   )
-}
-function base64toBlob(base64Data, contentType) {
-  contentType = contentType || ''
-  var sliceSize = 1024
-  var byteCharacters = atob(base64Data)
-  var bytesLength = byteCharacters.length
-  var slicesCount = Math.ceil(bytesLength / sliceSize)
-  var byteArrays = new Array(slicesCount)
-
-  for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-    var begin = sliceIndex * sliceSize
-    var end = Math.min(begin + sliceSize, bytesLength)
-
-    var bytes = new Array(end - begin)
-    for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
-      bytes[i] = byteCharacters[offset].charCodeAt(0)
-    }
-    byteArrays[sliceIndex] = new Uint8Array(bytes)
-  }
-  return new Blob(byteArrays, { type: contentType })
 }
