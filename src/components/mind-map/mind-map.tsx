55dc11c33654d0b05f3164865c3ba96e9c6b7f30
monichre <liamhellis@gmail.com>
2024-07-09 22:12:52 -0500

    adding get connections method to mindmap sidebar

--- src/components/mind-map/mind-map.tsx
+++ src/components/mind-map/mind-map.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useReportWebVitals } from 'next/web-vitals'
 import { Graph } from '@/features/graph/graph'
 
 import type { NetworkGraphPayload } from '@/lib/xata'
-import { UfologyProvider } from '@/providers/ufology-provider'
+
+import { GraphProvider } from '@/providers/graph-context'
+import { ReactFlowProvider } from '@xyflow/react'
 
 export interface MindMapProps {
   allEntityGraphData: NetworkGraphPayload['graphData']
@@ -13,14 +14,11 @@ export interface MindMapProps {
 export const MindMap: React.FC<MindMapProps> = ({
   allEntityGraphData,
 }: MindMapProps) => {
-  useReportWebVitals((metric) => {
-    console.log(metric)
-  })
   return (
-    <>
-      <UfologyProvider ufologyData={allEntityGraphData}>
+    <ReactFlowProvider>
+      <GraphProvider allEntityGraphData={allEntityGraphData}>
         <Graph />
-      </UfologyProvider>
-    </>
+      </GraphProvider>
+    </ReactFlowProvider>
   )
 }
--- src/providers/ufology-provider.tsx
+++ src/providers/ufology-provider.tsx
@@ -1,6 +1,33 @@
 'use client'
+import type { NetworkGraphPayload } from '@/lib/xata'
 import { GraphProvider } from '@/providers/graph-context'
+import { connections } from '@/stories/data/entity-data'
 import { ReactFlowProvider } from '@xyflow/react'
+import { createContext } from 'react'
+
+export type UfologyContextSchema = {
+  records: NetworkGraphPayload['records']
+  connections: NetworkGraphPayload['connections']
+  getConnections: any
+}
+
+export const UfologyContext: any = createContext({
+  records: {
+    topics: [],
+    events: [],
+    personnel: [],
+    testimonies: [],
+    organizations: [],
+  },
+  connections: {
+    topicsExpertsConnections: [],
+    eventsExpertsConnections: [],
+    eventsTopicsExpertsConnections: [],
+    topicsTestimoniesConnections: [],
+    organizationsPersonnelConnections: [],
+  },
+  getConnections: ({ id, type }: any) => {},
+})
 
 interface UfologyProvider {
   children: any
@@ -11,9 +38,32 @@ export const UfologyProvider: React.FC<UfologyProvider> = ({
   ufologyData,
   children,
 }) => {
+  const { records, connections } = ufologyData
+  const typeMap: any = {
+    events: 'event',
+    personnel: 'subject-matter-expert',
+    topics: 'topic',
+    organizations: 'organization',
+  }
+  const getConnections = ({ id, type }: any) => {
+    const singular: any = typeMap[type]
+    // @ts-ignore
+    const potentialConnections: any = [].concat(
+      [],
+      // @ts-ignore
+
+      ...Object.keys(connections)
+        .filter((key) => key.includes(type))
+        .map((key) => [...connections[key]])
+    )
+    const sources = potentialConnections.filter(
+      (potential: any) => potential[singular]?.id === id
+    )
+    console.log('sources: ', sources)
+  }
   return (
-    <ReactFlowProvider>
-      <GraphProvider allEntityGraphData={ufologyData}>{children}</GraphProvider>
-    </ReactFlowProvider>
+    <UfologyContext.Provider value={{ records, connections, getConnections }}>
+      {children}
+    </UfologyContext.Provider>
   )
 }
