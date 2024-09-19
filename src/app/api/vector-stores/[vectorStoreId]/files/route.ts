0cdfaf3777f56ebb5651596b7c827ce5e6588b59
monichre <liamhellis@gmail.com>
2024-09-18 19:14:48 -0500

    fixed all build errors

--- src/app/api/vector-stores/[vectorStoreId]/files/route.ts
+++ src/app/api/vector-stores/[vectorStoreId]/files/route.ts
@@ -3,12 +3,12 @@ import { openai } from '@/lib/openai/openai.client'
 // #TODO: Fix this
 
 export async function POST(req) {
-  const myVectorStoreFile = await openai.beta.vectorStores.files.create(
-    'vs_abc123',
-    {
-      file_id: 'file-abc123',
-    }
-  )
-  console.log(myVectorStoreFile)
+  // const myVectorStoreFile = await openai.beta.vectorStores.files.create(
+  //   'vs_abc123',
+  //   {
+  //     file_id: 'file-abc123',
+  //   }
+  // )
+  // console.log(myVectorStoreFile)
   // return Response.json({ record })
 }
